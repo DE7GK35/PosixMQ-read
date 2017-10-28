b065aa303e9e24a3dd3e04245747a9b79e58db1e
DE7GK35 <dfrancesconi@lycos.com>
2017-10-27 22:15:48 -0400

    Add files via upload

---
+++ PMQtest.js
@@ -0,0 +1,20 @@
+//var PosixMQ = require('posix-mq');
+var PosixMQ = require('./node_modules/posix-mq/lib/index');
+
+/* Create a new queue accessible by all, fill it up, and then close it. */
+var mq = new PosixMQ();
+mq.open({
+    name: '/events',
+    create: true,
+    mode: '0777',
+    maxmsgs: 10,
+    msgsize: 8
+});
+var writebuf = new Buffer(1);
+var r;
+do {
+    writebuf[0] = Math.floor(Math.random() * 93) + 33;
+    console.log("Writing "+ writebuf[0] +" ('"+ String.fromCharCode(writebuf[0]) +"') to the queue...");
+} while ((r = mq.push(writebuf)) !== false);
+    mq.unlink();
+    mq.close();
